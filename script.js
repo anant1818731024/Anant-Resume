94a50cb898c76ae7a6ba549582efbccf41bc164a
anant1818731024 <avashishtha2000@gmail.com>
2024-01-14 13:12:16 +0530

    added scroll effect

--- script.js
+++ script.js
@@ -12,4 +12,23 @@ const handleProjectClick = (project) => {
             urlToOpen = "https://github.com/anant1818731024/social-application"
     }
     if (urlToOpen) window.open(urlToOpen, "_blank");
-}   
+} 
+
+
+const handleNavClick = (e) => {
+    e.preventDefault();
+    const section = e?.target?.href?.split("#")[1];
+
+    const yCoordinate = document?.getElementById(section)?.getBoundingClientRect()?.y;
+
+    let currentScroll = 0;
+
+    const interval = setInterval(() => {
+        if(currentScroll >= yCoordinate){
+            clearInterval(interval);
+            return;
+        }
+        currentScroll += 10;
+        window.scrollBy(0, 10);
+    },5)
+}
